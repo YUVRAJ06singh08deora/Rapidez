de449b3942e683a95308a4fb422d08d1a1c10257
Yuvraj Singh Deora <06yuvraj2001singh@gmail.com>
2023-01-18 11:38:05 +0530

    Firebase Data Fetching Activity Added

--- app/src/main/java/com/example/rapidez/MainActivity.java
+++ app/src/main/java/com/example/rapidez/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.rapidez;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+private TextView clickbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        clickbtn=findViewById(R.id.clickme);
+        clickbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(), Showaccl_data.class);
+                startActivity(intent);
+            }
+        });
     }
 }
