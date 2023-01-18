165c548d67b2b8c673634193557f488436447532
Yuvraj Singh Deora <06yuvraj2001singh@gmail.com>
2023-01-18 17:02:09 +0530

    Main Activity UI added

--- app/src/main/java/com/example/rapidez/MainActivity.java
+++ app/src/main/java/com/example/rapidez/MainActivity.java
@@ -1,35 +1,21 @@
 package com.example.rapidez;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 private TextView clickbtn,mapbtn,location;
+LinearLayout lastLocation,performance,history;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        clickbtn=findViewById(R.id.clickme);
-        mapbtn=findViewById(R.id.mapactivity);
+        lastLocation=findViewById(R.id.lastlocation);
+        performance=findViewById(R.id.performance);
+        history=findViewById(R.id.history);
 
-
-        clickbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(), Driverss_performanceActivity.class);
-                startActivity(intent);
-            }
-        });
-        mapbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(),LastlocationActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
