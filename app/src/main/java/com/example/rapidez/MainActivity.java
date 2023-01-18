aed443df4e1d0e1be1ef991261395966b48808ec
Yuvraj Singh Deora <06yuvraj2001singh@gmail.com>
2023-01-18 16:42:43 +0530

    Map Activity Added

--- app/src/main/java/com/example/rapidez/MainActivity.java
+++ app/src/main/java/com/example/rapidez/MainActivity.java
@@ -1,23 +1,33 @@
 package com.example.rapidez;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
-private TextView clickbtn;
+private TextView clickbtn,mapbtn,location;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         clickbtn=findViewById(R.id.clickme);
+        mapbtn=findViewById(R.id.mapactivity);
+
+
         clickbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(getApplicationContext(), Driverss_performanceActivity.class);
+                startActivity(intent);
+            }
+        });
+        mapbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),LastlocationActivity.class);
                 startActivity(intent);
             }
         });
